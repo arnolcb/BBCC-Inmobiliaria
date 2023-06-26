2f3eea506ce693504934b5f8ee38475bf99229d9
Arnol Cáceres <116393375+arnolcb@users.noreply.github.com>
2023-06-25 22:40:06 -0500

    $user removido

--- php/login_usuario_be.php
+++ php/login_usuario_be.php
@@ -3,15 +3,12 @@
     session_start();
 
     include 'conexion_be.php';
-
-    $user = $_POST['user'];
     $mail = $_POST['mail'];
     $password = $_POST['password'];
 
     $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE mail='$mail' and password='$password'");
      
     if(mysqli_num_rows($validar_login) > 0){
-        $_SESSION['user'] = $user;
         $_SESSION['mail'] = $mail;
         header("location: ../main.php");
         exit;
